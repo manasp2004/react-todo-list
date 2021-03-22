5c5b7114edb156f3b6bcfc782999b91d0cb1d4c8
Manas Prakash <manasp2004@gmail.com>
2021-03-22 16:19:19 +0530

    Project Finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,29 @@ import "./App.css";
 
 export default function App() {
   const [todoInput, setTodoInput] = useState("");
-  const [id, setId] = useState(0)
-  const [todos, setTodos]: any = useState([])
+  const [id, setId] = useState(0);
+  const [todos, setTodos]: any = useState([]);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     if (todoInput === "") return;
-    setTodos((todo: any) => [...todo, todoInput])
-    console.log(todos)
+    setTodos((todo: any) => [...todo, todoInput]);
+    console.log(todos);
     setTodoInput("");
     setId(id + 1);
 
     // console.log(e)
   };
+
+  const handleDelete = (e: any) => {
+    e.preventDefault();
+    console.log(e);
+    e.target.style.color = "rgb(30, 30, 30)";
+  };
+
   return (
     <div className='App'>
+      <h1>To-Do List</h1>
       <form onSubmit={handleSubmit}>
         <input
           type='text'
@@ -29,9 +37,12 @@ export default function App() {
 
       <ul>
         {todos.map((todo: string) => (
-          <li key={id}>{todo}</li>
+          <li key={id} onClick={handleDelete}>
+            {todo}
+          </li>
         ))}
       </ul>
+      <button className="btn" onClick={() => setTodos([])}>Reset</button>
     </div>
   );
 }
